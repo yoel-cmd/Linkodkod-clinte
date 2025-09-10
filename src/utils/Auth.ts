15540ecd0e80be4e401d404607012e9b3feb3fae
yoel ider <yoeli5746@gmail.com>
2025-09-10 17:40:30 +0300

    finish

--- src/utils/Auth.ts
+++ src/utils/Auth.ts
@@ -1,41 +1,7 @@
-// import { insertTokenToLS } from "./LocalStoreg";
-
-// {/*A function that makes a fetch request to the server to get all posts */}
-// export async function Login(body:any) {
-//     console.log("body",body);
-    
-//   try {
-//    const res:any = await fetch(`http://localhost:3000/login`, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(body),
-//   });
-//         console.log("res:",res);
-        
-//     if (res.available) {
-//         insertTokenToLS("auth",JSON.stringify(body))
-//         console.log("Saved locally");
-//       return true
-//     }
-
-//     return false;
-//   } catch (err) {
-//     console.error(" err:", err);
-//     throw err;
-//   }
-// }
-
-
-
-
-
-
-
-
+//---------------------------------------------imports----------------------------------------------
 import { insertTokenToLS } from "./LocalStoreg";
 
+//---------------------------------------------start function--------------------------------------------
 export async function Login(body: any) {
   console.log("body", body);    
 
--- src/utils/CRUD.ts
+++ src/utils/CRUD.ts
@@ -15,31 +15,15 @@ export async function readAllPost() {
   }
 }
 
-// export async function readAllPostAuth(user:any) {
-//   const res:any = await fetch(`http://localhost:3000/post`, {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify({ username: user.username, password: user.password }),
-//   });
-//   if (!res.ok) throw new Error("Failed to load posts",res.status);
-//   return (await res.json()) as any[];
-// }
-
-// {/*A function that makes a fetch request to the server to get a post by ID. */}
-// export async function readPostById(id: any) {
-//   try {
-//     const res = await fetch(`http://localhost:3000/post/${id}`);
-//     if (!res.ok) {
-//       const err = await res.json();
-//       throw new Error(err.error);
-//     }
-//     return await res.json();
-//   } catch (err) {
-//     console.error(" err:", err);
-//     throw err;
-//   }
-// }
-
+export async function readAllPostAuth(user:any) {
+  const res:any = await fetch(`http://localhost:3000/post`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username: user.username, password: user.password }),
+  });
+  if (!res.ok) throw new Error("Failed to load posts",res.status);
+  return (await res.json()) as any[];
+}
 
 
 export async function readPostById(id: string, user:any ) {
