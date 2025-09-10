c050adc97e4ea0ed42086d1bdd2dafaeed7d5cab
yoel ider <yoeli5746@gmail.com>
2025-09-10 13:00:18 +0300

    readPostById

--- src/utils/CRUD.ts
+++ src/utils/CRUD.ts
@@ -15,28 +15,39 @@ export async function readAllPost() {
 }
 
 
-{/*A function that makes a fetch request to the server to get a post by ID. */}
-export async function readPostById(id: any) {
-  try {
-    const res = await fetch(`http://localhost:3000/post/${id}`);
-    if (!res.ok) {
-      const err = await res.json();
-      throw new Error(err.error);
-    }
-    return await res.json();
-  } catch (err) {
-    console.error(" err:", err);
-    throw err;
-  }
-}
+// {/*A function that makes a fetch request to the server to get a post by ID. */}
+// export async function readPostById(id: any) {
+//   try {
+//     const res = await fetch(`http://localhost:3000/post/${id}`);
+//     if (!res.ok) {
+//       const err = await res.json();
+//       throw new Error(err.error);
+//     }
+//     return await res.json();
+//   } catch (err) {
+//     console.error(" err:", err);
+//     throw err;
+//   }
+// }
 
 export async function readAllPostAuth(user:any) {
-  const res = await fetch(`http://localhost:3000/post`, {
+  const res:any = await fetch(`http://localhost:3000/post`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ username: user.username, password: user.password }),
   });
-  if (!res.ok) throw new Error(`Failed to load posts (${res.status})`);
+  if (!res.ok) throw new Error("Failed to load posts",res.status);
   return (await res.json()) as any[];
+}
+
+
+export async function readPostById(id: string, user:any ) {
+  const res:any = await fetch(`http://localhost:3000/post/${id}`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username: user.username, password: user.password }),
+  });
+  if (!res.ok) throw new Error("Failed to load posts",res.status);
+  return (await res.json()) as any;
 }
 
